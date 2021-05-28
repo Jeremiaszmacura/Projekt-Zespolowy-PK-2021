92fe73621af1e6bee55135faadf1ef5b86edc206
Michal Kwiecinski <mikwiecinski99@gmail.com>
2021-05-28 15:51:10 +0200

    fixes

--- frontend/src/routes/AddProduct.js
+++ frontend/src/routes/AddProduct.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "./Login.css"
 import axios from "axios";
 import authentication from "../scripts/authentication";
