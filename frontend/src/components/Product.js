a9dda9c8982356e9a0ae4eaeb57a62f9303b259e
Kamil Maksymowicz <maxio090124@interia.pl>
2021-05-29 19:45:38 +0200

    fetch products data from Api

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,22 +1,32 @@
 import exampleData from './exampleData';
+import React, {useState, useEffect} from "react";
 import { useHistory } from 'react-router-dom';
 import "./Product.css";
+import useFetch from "../services/useFetch";
 
 const Product = (props) => {
+    const productsURL = "https://http://localhost:4000/";
+
     const history = useHistory();
+    let fetchFun = useFetch(productsURL,{});
+    const [productsData, setProductsData] = useState();
     function handleClick(id){
         history.push('/product-details', {params: id})
     }
+    
+    setProductsData(fetchFun[0]);
+    
 
     return (  
         <div className="productContent">
-            {exampleData.products
+            {productsData?
+            productsData
                 .filter(p =>
-                    p.cena <= props.cena.max
-                    && p.cena >= props.cena.min
+                    p.price <= props.cena.max
+                    && p.price >= props.cena.min
                 )
                 .filter(p =>
-                    !props.query && true || (p.produkt + ' ' + p.marka + ' ' + p.nazwa).toLowerCase().includes(props.query.toLowerCase())
+                    !props.query && true || (p.category + ' ' + p.mark + ' ' + p.name).toLowerCase().includes(props.query.toLowerCase())
                 )
                 .map((e) =>
             {   
@@ -24,19 +34,19 @@ const Product = (props) => {
                 <a className="button" onClick={() => handleClick(e.id)}>
                     <div className="productCard" key={e.id}>
                         <div className="img">
-                            <img className="medium" src ={e.image} alt="productimg"/>
+                            <img className="medium" src ="/images/MaCentre.png" alt="productimg"/>
                         </div>
                         <div className="info">
-                            <div className="marka">{e.produkt} {e.marka}</div>
-                            <div className="nazwa">{e.nazwa}</div>
-                            <div className="price">Cena: {e.cena}zł</div>
+                            <div className="marka">{e.category} {e.mark}</div>
+                            <div className="nazwa">{e.name}</div>
+                            <div className="price">Cena: {e.price}zł</div>
                         </div>
                     </div>
                 </a>
                 : 
                 null;
                 }
-            )}
+            ):<div>NIE MA I NIE BEDZIE</div>}
         </div>
     );
 }
--- frontend/src/components/ProductDetails.js
+++ frontend/src/components/ProductDetails.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 import { useLocation } from "react-router-dom";
 import exampleData from './exampleData';
 import './ProductDetails.css'
+import useFetch from "../services/useFetch";
 
 const ProductDetails = () => {
     const location = useLocation();
     const myid = location.state.params;
     const ilosc = 5;
     const [amount, setAmount] = useState(1);
+    const [singleProduct, setSingleProduct] = useState([]);
+    const productsURL = `https://http://localhost:4000/${myid}`;
+
+    let fetchFun = useFetch(productsURL,{});
+
+    setSingleProduct(fetchFun[0]);
 
     function handleClick(prop){
         if(amount >= 0 && amount < ilosc){
@@ -18,13 +25,13 @@ const ProductDetails = () => {
     
     return (
         <div className="detailsComponent">
-            {exampleData.products.map((e) => {
+            {singleProduct.map((e) => {
                 return myid === e.id ? 
                 <div>
                 <div className="product">
-                    <img src ={e.image} alt="productimg"/>
+                    <img src ="/images/MaCentre.png" alt="productimg"/>
                     <div className="productName">
-                        <div className="fullName">{e.marka} {e.nazwa}</div>
+                        <div className="fullName">{e.mark} {e.name}</div>
                         <div className="productDetails">
                             <div className="if">Info1: ZZZZZ</div>
                             <div className="if">Info2: YYYY</div>
@@ -34,7 +41,7 @@ const ProductDetails = () => {
                         </div>
                     </div>
                     <div className="priceDetails">
-                        <div className="cena">{e.cena}zł</div>
+                        <div className="cena">{e.price}zł</div>
                         <div className="dostepnosc">Dostępnych sztuk: {ilosc}</div>
                         <div className="amount">
                             <div>Wybierz ilosc: {amount}</div>
