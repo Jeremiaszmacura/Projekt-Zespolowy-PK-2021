e8c85b08a1bc502a582756baae148be62b325749
dkotula <dkotula.wieliczka@gmail.com>
2021-05-30 17:02:02 +0200

    Return more info about product in orders

--- back-end/src/entity/Order.ts
+++ back-end/src/entity/Order.ts
@@ -16,7 +16,10 @@ export class Order {
     })
     order_status: OrderStatus;
 
-    @Column('double precision')
+    @Column({
+        type: "double precision",
+        nullable: true
+    })
     price: number;
 
     @Column()
