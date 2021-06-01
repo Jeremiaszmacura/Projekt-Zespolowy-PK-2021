01721965db7657097178c403e833c2ff9bf4b56d
Michal Kwiecinski <mikwiecinski99@gmail.com>
2021-06-01 11:20:01 +0200

    Fixes

--- frontend/src/routes/AddProduct.js
+++ frontend/src/routes/AddProduct.js
@@ -57,7 +57,7 @@ const AdminSite = () => {
                 if(token !== "blad"){
                     alert("Dodales produkt pomyslnie");
                 } else {
-                    throw Error("NIe dodano produktu");
+                    throw Error("Nie dodano produktu");
                 }
             })
             .catch(res =>{
@@ -83,9 +83,10 @@ const AdminSite = () => {
                     <input className="loginRegisterFormInput" type="number" name="price" id="price" onChange={e => setPrice(e.target.value)}  />
                     <p>Kategoria</p>
                     <select className="loginRegisterFormInput" value={category} onChange={e => setCategory(e.target.value)} >
-                        <option value="1" >Laptop</option>
-                        <option value="2" >Telefon</option>
+                        <option value="1" >Komputer</option>
+                        <option value="2" >Laptop</option>
                         <option value="3" >Tablet</option>
+                        <option value="4" >Telefon</option>
                     </select>
                     <p>Zdjęcie</p>
                     <input className="loginRegisterFormInput" type="file" name="file" id="file" accept="image/x-png,image/jpeg" onChange={handleFile} />
